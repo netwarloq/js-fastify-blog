21232163a14f40fe762480b06e1bfa7d3261be8d
NetWarloq <netwarloq@gmail.com>
2023-07-10 01:16:32 +0500

    fix badget

--- README.md
+++ README.md
@@ -1,6 +1,5 @@
 ### Hexlet tests and linter status:
-[![Actions Status](https://github.com/netwarloq/devops-for-programmers-project-74/workflows/hexlet-check/badge.svg)](https://github.com/netwarloq/devops-for-programmers-project-74/actions)
-[![Test and Build Status](https://github.com/netwarloq/devops-for-programmers-project-74/actions/workflows/push.yml/badge.svg)](https://github.com/netwarloq/devops-for-programmers-project-74/actions)
+[![Test and Build Status](https://github.com/netwarloq/js-fastify-blog/actions/workflows/push.yml/badge.svg)](https://github.com/netwarloq/js-fastify-blog/actions)
 
 Image on DockerHub: netwarloq/devops-for-programmers-project-74
 
--- app/config/config.cjs
+++ app/config/config.cjs
@@ -19,12 +19,16 @@ module.exports = {
     //   },
     // },
   },
-  test: {
-    dialect: 'postgres',
-    database: process.env.DATABASE_NAME,
-    username: process.env.DATABASE_USERNAME,
-    password: process.env.DATABASE_PASSWORD,
-    port: process.env.DATABASE_PORT,
-    host: process.env.DATABASE_HOST,
+  development: {
+    dialect: 'sqlite',
+    storage: './database.sqlite',
   },
+  // test: {
+  //   dialect: 'postgres',
+  //   database: process.env.DATABASE_NAME,
+  //   username: process.env.DATABASE_USERNAME,
+  //   password: process.env.DATABASE_PASSWORD,
+  //   port: process.env.DATABASE_PORT,
+  //   host: process.env.DATABASE_HOST,
+  // },
 };
